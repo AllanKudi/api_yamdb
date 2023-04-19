e7c7d78562e389561fb0162964905d1774feca14
Виталий Глазков <viusglazkov@yandex.ru>
2023-04-19 14:40:19 +0500

    new api

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,30 +1,39 @@
 from rest_framework import permissions
 
 
-class AdminOnly(permissions.BasePermission):
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """Даёт доступ неадмину только к GET/OPTIONS/HEAD."""
+
+    message = 'Данный запрос недоступен для вас.'
+
+    def has_permission(self, request, view):
+        """Проверка на запросы к объекту
+        Для безопасных методов всегда True."""
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_admin)
+
+
+class IsAdminUser(permissions.BasePermission):
+    """Доступ только для пльзователей с ролью администратора."""
+
+    message = 'Данный запрос недоступен для вас.'
+
     def has_permission(self, request, view):
         return request.user.is_admin
 
 
-class IsAdminUserOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated and request.user.is_admin
-        )
+class IsAuthorOrModerAdminPermission(permissions.BasePermission):
+    """Даёт доступ неадмину/немодеру/неавтору только к GET/OPTIONS/HEAD."""
 
-
-class AllPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+    message = 'Данный запрос недоступен для вас.'
 
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-            or request.user.is_moderator
-            or request.user.is_admin
+            or request.user.is_authenticated
+            and (
+                request.user.is_admin
+                or request.user.is_moderator
+                or request.user == obj.author
+            )
         )
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,8 +1,9 @@
 import os
+from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
@@ -24,9 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'api',
-    'reviews',
-    'user',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
